4d47f03ff9d344954694b6eb5042d70a0150b411
Tharana Mayuranga <tharana.maya@gmail.com>
2018-03-01 22:37:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -409,5 +409,20 @@
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
       );
     }
+      function callbackFunc() {
+      for (var i = 0; i < items.length; i++) {
+        if (isElementInViewport(items[i])) {
+          items[i].classList.add("in-view");
+        }
+      }
+    }
+
+    // listen for events
+    window.addEventListener("load", callbackFunc);
+    window.addEventListener("resize", callbackFunc);
+    window.addEventListener("scroll", callbackFunc);
+
+  })();
+  </script>
   </body>
   </html>
