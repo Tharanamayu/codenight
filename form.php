291442b8a9a393594ef6157e036e29465adea873
Tharana Mayuranga <tharana.maya@gmail.com>
2018-03-04 22:15:35 +0530

    Update form.php

--- form.php
+++ form.php
@@ -9,6 +9,14 @@
 	</script>
 </head>
 <body bgcolor=>
+<div class="login-page">
+  <div class="form">
+  	   <h2>Code Night Registration</h2> <br>
+      <a href="form1/form1.php"><button>Idea Hackathon</button></a>
+      <br> <br>
+      <a href="form/form.php"><button>Algorithm Hackathon</button></a>
 
+  </div>
+</div>
 </body>
 </html>
