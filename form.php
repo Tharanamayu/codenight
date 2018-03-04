b69c2800f06e44f4529489966c81852e8cc6a1c4
Tharana Mayuranga <tharana.maya@gmail.com>
2018-03-04 22:14:38 +0530

    Create form.php

---
+++ form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration</title>
+	<script>
+		$('.message a').click(function(){
+   		$('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+			});
+	</script>
+</head>
+<body bgcolor=>
+
+</body>
+</html>
