c4b6fe16f71184f6e2717f04dbf5237733fb6873
Tharana Mayuranga <tharana.maya@gmail.com>
2018-03-04 22:17:24 +0530

    Update form.php

--- form.php
+++ form.php
@@ -18,5 +18,59 @@
 
   </div>
 </div>
+	<style>
+a{
+	text-decoration: none;
+}
+	@import url(https://fonts.googleapis.com/css?family=Roboto:300);
+
+.login-page {
+  width: 380px;
+  padding: 8% 0 0;
+  margin: auto;
+}
+.form {
+  position: relative;
+  z-index: 1;
+  background: #867979;
+  max-width: 360px;
+  margin: 0 auto 100px;
+  padding: 45px;
+  text-align: center;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+}
+
+.form button {
+  font-family: "Roboto", sans-serif;
+  text-transform: uppercase;
+  outline: 0;
+  background: #2c2d66;
+  width: 100%;
+  border: 0;
+  padding: 15px;
+  color: #FFFFFF;
+  font-size: 14px;
+  -webkit-transition: all 0.3 ease;
+  transition: all 0.3 ease;
+  cursor: pointer;
+}
+.form button:hover,.form button:active,.form button:focus {
+  background: #434c99;
+}
+
+body {
+	background-image: url("img/img.jpg");
+}
+  background-image: url("img.jpg");
+  background-color: #808080; /* fallback for old browsers */
+  background: -webkit-linear-gradient(right, #76b852, #808080);
+  background: -moz-linear-gradient(right, #76b852, #8DC26F);
+  background: -o-linear-gradient(right, #76b852, #8DC26F);
+  background: linear-gradient(to left, #76b852, #8DC26F);
+  font-family: "Roboto", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
 </body>
 </html>
