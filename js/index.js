de95c6d7c4013127e5a332b626ca186fd7804138
Tharanamayu <tharana.maya@gmail.com>
2018-03-06 22:48:47 +0530

    Added index.js file

--- js/index.js
+++ js/index.js
@@ -22,11 +22,11 @@
     displacement = 140,
     power = Math.pow(2,Math.ceil(Math.log(width)/(Math.log(2))));
 
-  // set the start height and end height for the terrain
+
   points[0] = (height - (Math.random()*height/2))-displacement;
   points[power] = (height - (Math.random()*height/2))-displacement;
 
-  // create the rest of the points
+
   for(var i = 1; i<power; i*=2){
     for(var j = (power/i)/2; j <power; j+=power/i){
       points[j] = ((points[j - (power/i)/2] + points[j + (power/i)/2]) / 2) + Math.floor(Math.random()*-displacement+displacement );
@@ -50,7 +50,7 @@
 	terCtx.fill();
 
 
-	// Second canvas used for the stars
+
 	bgCtx.fillStyle = '#05004c';
 	bgCtx.fillRect(0,0,width,height);
 
@@ -88,7 +88,7 @@
 		this.len = (Math.random()*80)+10;
 		this.speed = (Math.random()*10)+6;
 		this.size = (Math.random()*1)+0.1;
-    // this is used so the shooting stars arent constant
+
 		this.waitTime =  new Date().getTime() + (Math.random()*3000)+500;
 		this.active = false;
 	}
@@ -114,11 +114,28 @@
 	}
 
 	var entities = [];
-  // init the stars
+  //  stars
   for(var i=0; i < height; i++){
     entities.push(new Star({x:Math.random()*width, y:Math.random()*height}));
   }
 
-  // Add 2 shooting stars that just cycle.
+  // Add 2 shooting stars
   entities.push(new ShootingStar());
   entities.push(new ShootingStar());
+
+  //animate background
+	function animate(){
+		bgCtx.fillStyle = '#05004c';
+		bgCtx.fillRect(0,0,width,height);
+		bgCtx.fillStyle = '#ffffff';
+		bgCtx.strokeStyle = '#ffffff';
+
+		var entLen = entities.length;
+
+		while(entLen--){
+			entities[entLen].update();
+		}
+
+		requestAnimationFrame(animate);
+	}
+	animate();
