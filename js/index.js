7521f948a2403a40b24636f9a4b25b72f84f4361
Tharanamayu <tharana.maya@gmail.com>
2018-03-06 22:44:13 +0530

    Added index.js file

--- js/index.js
+++ js/index.js
@@ -33,3 +33,84 @@
     }
     displacement *= 0.6;
   }
+  // draw the terrain
+	terCtx.beginPath();
+
+	for(var i = 0; i<=width; i++){
+		if(i === 0){
+			terCtx.moveTo(0, points[0]);
+		}else if(points[i] !== undefined){
+			terCtx.lineTo(i, points[i]);
+		}
+	}
+
+	terCtx.lineTo(width,terrain.height);
+	terCtx.lineTo(0,terrain.height);
+	terCtx.lineTo(0,points[0]);
+	terCtx.fill();
+
+
+	// Second canvas used for the stars
+	bgCtx.fillStyle = '#05004c';
+	bgCtx.fillRect(0,0,width,height);
+
+	// stars
+	function Star(options){
+		this.size = Math.random()*2.5;
+		this.speed = Math.random()*.4;
+		this.x = options.x;
+		this.y = options.y;
+	}
+
+	Star.prototype.reset = function(){
+		this.size = Math.random()*2.5;
+		this.speed = Math.random()*.4;
+		this.x = width;
+		this.y = Math.random()*height;
+	}
+
+	Star.prototype.update = function(){
+		this.x-=this.speed;
+		if(this.x<0){
+		  this.reset();
+		}else{
+		  bgCtx.fillRect(this.x,this.y,this.size,this.size);
+		}
+	}
+
+	function ShootingStar(){
+		this.reset();
+	}
+
+	ShootingStar.prototype.reset = function(){
+		this.x = Math.random()*width;
+		this.y = 0;
+		this.len = (Math.random()*80)+10;
+		this.speed = (Math.random()*10)+6;
+		this.size = (Math.random()*1)+0.1;
+    // this is used so the shooting stars arent constant
+		this.waitTime =  new Date().getTime() + (Math.random()*3000)+500;
+		this.active = false;
+	}
+
+	ShootingStar.prototype.update = function(){
+		if(this.active){
+			this.x-=this.speed;
+			this.y+=this.speed;
+			if(this.x<0 || this.y >= height){
+			  this.reset();
+			}else{
+			bgCtx.lineWidth = this.size;
+				bgCtx.beginPath();
+				bgCtx.moveTo(this.x,this.y);
+				bgCtx.lineTo(this.x+this.len, this.y-this.len);
+				bgCtx.stroke();
+			}
+		}else{
+			if(this.waitTime < new Date().getTime()){
+				this.active = true;
+			}
+		}
+	}
+
+	var entities = [];
