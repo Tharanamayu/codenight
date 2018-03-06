f88396918997d537a35933f46f93a1d383a807b9
Tharanamayu <tharana.maya@gmail.com>
2018-03-06 22:42:23 +0530

    Added index.js file

--- js/index.js
+++ js/index.js
@@ -17,3 +17,19 @@
 
 	terrain.width = background.width = width;
 	terrain.height = background.height = height;
+  // Some random points
+  var points = [],
+    displacement = 140,
+    power = Math.pow(2,Math.ceil(Math.log(width)/(Math.log(2))));
+
+  // set the start height and end height for the terrain
+  points[0] = (height - (Math.random()*height/2))-displacement;
+  points[power] = (height - (Math.random()*height/2))-displacement;
+
+  // create the rest of the points
+  for(var i = 1; i<power; i*=2){
+    for(var j = (power/i)/2; j <power; j+=power/i){
+      points[j] = ((points[j - (power/i)/2] + points[j + (power/i)/2]) / 2) + Math.floor(Math.random()*-displacement+displacement );
+    }
+    displacement *= 0.6;
+  }
